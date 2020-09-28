8b0245ed64d1741ea3c0927c2f0793c3557e59c5
Abdul Khan <akhan17@outlook.com>
2020-09-28 16:09:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function mapToNegativize(sourceArray){
 function mapToNoChange(sourceArray){
     return sourceArray.map(x => x);
 }
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(x => x*2);
+}
