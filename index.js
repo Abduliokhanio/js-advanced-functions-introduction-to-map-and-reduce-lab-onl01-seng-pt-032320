55827bb1a018fa875ce7197492bc6c6450d3f1bb
Abdul Khan <akhan17@outlook.com>
2020-09-28 16:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,3 +20,26 @@ function reduceToTotal(sourceArray, startingPoint = 0){
 
     return sourceArray.reduce(reducer, startingPoint)
 }
+
+function reduceToAllTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){  
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+
+function reduceToAnyTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){  
+        if(accumulator == true){
+            return true
+        } else {
+            return !!currentValue
+        } 
+    }
+    return sourceArray.reduce(reducer, false)
+} 
